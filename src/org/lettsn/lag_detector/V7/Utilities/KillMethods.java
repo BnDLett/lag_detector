deb7fcca90f3613c9c6e2f6a67723af949b4d85c
Lett <lett@mail.lettsn.org>
2025-06-09 20:30:45 -0400

    refactor: Change kill methods to be public

--- src/org/lettsn/lag_detector/V7/Utilities/KillMethods.java
+++ src/org/lettsn/lag_detector/V7/Utilities/KillMethods.java
@@ -10,26 +10,26 @@ public class KillMethods{
     // https://github.com/mindustry-ddns-net/MapUtils/blob/master/src/main/java/xyz/semetrix/maputils/Commands/KillUnits/KillUnitsCommand.java
     // thanks, semetrix
     // (sorry to any randoms that have no access to that repository)
-    static void runAll() {
+    public static void runAll() {
         removeAllTurrets();
         removeAllPayloadSources();
         killAllUnits();
     }
 
-    static void killAllUnits() {
+    public static void killAllUnits() {
         Seq<Unit> units = Groups.unit.copy(new Seq<>()).select(Unitc::isAI);
 
         units.forEach(Unitc::kill);
     }
 
-    static void removeAllPayloadSources() {
+    public static void removeAllPayloadSources() {
         // weird java black magic. just know that it creates a sequence of all payload sources.
         Seq<Building> buildings = Groups.build.copy(new Seq<>()).select(building -> building.block == Blocks.payloadSource);
 
         buildings.forEach(Building::kill);
     }
 
-    static void removeAllTurrets() {
+    public static void removeAllTurrets() {
         Seq<Building> turrets = Groups.build.copy(new Seq<>()).select(building ->
                 building.block.group == BlockGroup.turrets);
 
